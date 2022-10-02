2719dcbc1258a0c927c675fe86bc138529567d78
Guzeva-Julia <yuguzeva@gmail.com>
2022-10-02 18:52:46 +0300

    Java
    Java

---
+++ .idea/Mod.java
@@ -0,0 +1,12 @@
+public class Mod {
+    public static void main(String[] args) {
+        int x = 13;
+        int y = 151;
+
+        int lastNumberX = x%10;
+        int lastNumberY = y%10;
+
+        System.out.println("Остання цифра значення Х буде " + lastNumberX);
+        System.out.println("Остання цифра значення Y буде " + lastNumberY);
+    }
+}
